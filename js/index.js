942e270c47541f3b42be27b2d0d9270896f63f59
Olha Suprunova <suprunovaolha@gmail.com>
2019-05-08 19:21:11 +0000

    Description of the work done

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.querySelector('.yourinfo input[type=submit]').addEventListener('click', 
+  submitForm);
+  function submitForm(e) {
+      document.forms["yourinfo"].submit();
+    e.preventDefault();
+}
